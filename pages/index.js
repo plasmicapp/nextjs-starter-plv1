clovis1122/nextjs-starter-blog

Adapt starter for Plasmic

pages/index.js
@@ -6,33 +6,40 @@ import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
+import PlasmicLoader from '@plasmicapp/loader'
 
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+
+  const containerContents = allPosts.length === 0 ? (
+    <p>
+      No blog posts found. Add markdown posts to "_posts".
+    </p>
+  ) : allPosts.map(post => (
+    <PlasmicLoader
+      component={"ListItem"}
+      componentProps={{
+        title: post.title,
+        date: post.date,
+        description: post.excerpt,
+        href: "/posts/" + post.slug,
+      }}
+    />
+  ));
+
+  return <PlasmicLoader
+    component={"Homepage"}
+    componentProps={{
+      container: {
+        children: containerContents,
+      },
+    }}
+    providerProps={{
+      Screen: null
+    }}
+  />;
+
 }
 
 export async function getStaticProps() {

pages/posts/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
+import PlasmicLoader from "@plasmicapp/loader";
 import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import Header from '../../components/header'
@@ -16,34 +17,34 @@ export default function Post({ post, morePosts, preview }) {
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
-  return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
-    </Layout>
-  )
+
+  return (<PlasmicLoader 
+    component="PostPage"
+    componentProps={{
+      postHeader: {
+        postTitle: post.title,
+        date: post.date
+      },
+      container: {
+        render: () => (
+          <PostBody content={post.content} />
+        )
+      },
+      previousPost: {
+        style: {
+          visibility: "hidden",
+        },
+      },
+      nextPost: {
+        style: {
+          visibility: "hidden",
+        },
+      }}
+    }
+    providerProps={{
+      Screen: null
+    }}
+  />);
 }
 
 export async function getStaticProps({ params }) {

plasmicapp/nextjs-starter-plv1

pages/index.js
@@ -6,33 +6,37 @@ import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
+import PlasmicLoader from '@plasmicapp/loader'
 
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
+
+  const containerContents = allPosts.length === 0 ? (
+    <p>
+      No blog posts found. Add markdown posts to "_posts".
+    </p>
+  ) : allPosts.map(post => (
+    <PlasmicLoader
+      component={"ListItem"}
+      componentProps={{
+        title: post.title,
+        date: post.date,
+        description: post.excerpt,
+        href: "/posts/" + post.slug,
+      }}
+    />
+  ));
+
+  return <PlasmicLoader
+    component={"Home"}
+    componentProps={{
+      container: {
+        children: containerContents,
+      },
+    }}
+  />;
+
 }
 
 export async function getStaticProps() {

pages/posts/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
+import PlasmicLoader from "@plasmicapp/loader";
 import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import Header from '../../components/header'
@@ -16,34 +17,31 @@ export default function Post({ post, morePosts, preview }) {
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
-  return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
-          <PostTitle>Loading…</PostTitle>
-        ) : (
-          <>
-            <article className="mb-32">
-              <Head>
-                <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
-                </title>
-                <meta property="og:image" content={post.ogImage.url} />
-              </Head>
-              <PostHeader
-                title={post.title}
-                coverImage={post.coverImage}
-                date={post.date}
-                author={post.author}
-              />
-              <PostBody content={post.content} />
-            </article>
-          </>
-        )}
-      </Container>
-    </Layout>
-  )
+
+  return (<PlasmicLoader 
+    component="Post"
+    componentProps={{
+      postHeader: {
+        postTitle: post.title,
+        date: post.date
+      },
+      container: {
+        render: () => (
+          <PostBody content={post.content} />
+        )
+      },
+      previousPost: {
+        style: {
+          visibility: "hidden",
+        },
+      },
+      nextPost: {
+        style: {
+          visibility: "hidden",
+        },
+      }}
+    }
+  />);
 }
 
 export async function getStaticProps({ params }) {